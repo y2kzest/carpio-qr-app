f3bb12da281656d1c8a25b220c8987f8bffa516b
ohio080500 <mileslaurencejubinal@yahoo.com>
2025-06-20 10:34:57 +0800

    added login page

--- layouts/default.vue
+++ layouts/default.vue
@@ -101,11 +101,6 @@ export default {
           icon: 'mdi-apps',
           title: 'Welcome',
           to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
         }
       ],
       miniVariant: false,
